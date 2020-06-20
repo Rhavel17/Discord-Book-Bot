eede137cfb94d9509b87d4a51b7443746df518ba
Ryan Havel <ryanhavel14@gmail.com>
2020-06-20 18:51:23 -0400

    Added comments. Added 'on_message' client.event

--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,29 @@
-# bot.py
 import os
-
-import discord
 from dotenv import load_dotenv
+import discord
 
+# Load environment variable(s) from .env file to shell's environment variables
 load_dotenv()
+
+# Get the value of the 'DISCORD_TOKEN' env variable
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Create instance of Client (represents client connection to Discord)
 client = discord.Client()
 
+# Called when 'client is done preparing the data received from discord
+# Usually after login is successful and the Client.guilds are filled up'
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('We have logged in as {0.user}'.format(client))
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+# Run the Client
 client.run(TOKEN)
