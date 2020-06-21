efd22e9e29d82808139e48be4492f82cd3331b7a
Ryan Havel <ryanhavel14@gmail.com>
2020-06-20 21:55:56 -0400

    Added test, shutdown, and WRG commands

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-import discord
+from discord.ext import commands
 
 # Load environment variable(s) from .env file to shell's environment variables
 load_dotenv()
@@ -8,22 +8,60 @@ load_dotenv()
 # Get the value of the 'DISCORD_TOKEN' env variable
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Create instance of Client (represents client connection to Discord)
-client = discord.Client()
+# Create instance of Bot
+bot = commands.Bot(command_prefix='!')
+
+# List of global variables w/ default values
+wrg = None
 
 # Called when 'client is done preparing the data received from discord
 # Usually after login is successful and the Client.guilds are filled up'
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {bot.user.name}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+# '!test' command
+@bot.command(name='test', help='Repeats given arguments')
+async def test(ctx, *args):
+    await ctx.send('{0} arguments: {1}'.format(len(args), ', '.join(args)))
+
+
+# '!shutdown' command
+@bot.command(name='shutdown', help='Turns the bot off')
+async def shutdown(ctx, *args):
+    await ctx.send('Shutting down Discord Book Bot')
+    exit()
+
+
+# '!setWRG' command
+@bot.command(name='setWRG', help='SET Weekly Reading Goal')
+async def setWRG(ctx, goal: str):
+    # Overwrite 'wrg' global variable
+    global wrg
+    wrg = goal
+
+    # Send confirmation message to discord
+    await ctx.send('Weekly Reading Goal set to \'{}\''.format(goal))
+
+
+# '!getWRG' command
+@bot.command(name='getWRG', help='GET Weekly Reading Goal')
+async def getWRG(ctx):
+    if wrg is None:
+        await ctx.send('Weekly Reading Goal has not been set')
+    else:
+        await ctx.send('Weekly Reading Goal: \'{}\''.format(wrg))
+
+
+# '!deleteWRG' command
+@bot.command(name='deleteWRG', help='DELETE Weekly Reading Goal')
+async def deleteWRG(ctx):
+    # Overwrite 'wrg' global variable
+    global wrg
+    wrg = None
+
+    await ctx.send('Removed Weekly Reading Goal')
 
 # Run the Client
-client.run(TOKEN)
+bot.run(TOKEN)
