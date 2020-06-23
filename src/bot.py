c93bf44f34852559b437ef76e6845ce4e84da6d2
Ryan Havel <ryanhavel14@gmail.com>
2020-06-22 23:12:29 -0400

    Exception Handling

--- src/bot.py
+++ src/bot.py
@@ -20,6 +20,43 @@ book = None
 async def on_ready():
     print(f'We have logged in as {bot.user.name}')
 
+# Exception Handling for the bot
+@bot.event
+async def on_command_error(ctx, err):
+    # Ignore any errors that originate in on_command_error
+    if hasattr(ctx.command, 'on_error'):
+        return
+
+    if isinstance(err, commands.MissingRequiredArgument):
+        return await ctx.send('Cannot grant your request. Missing a parameter for \'{}\'.'.format(ctx.command.name))
+    elif isinstance(err, commands.ArgumentParsingError):
+        return await ctx.send('Cannot grant your request. Unable to parse your input for {}.'.format(ctx.command.name))
+    elif isinstance(err, commands.BadArgument):
+        return await ctx.send('Cannot grant your request. Parsing or conversion failure on command argument(s) for {}.'.format(ctx.command.name))
+    elif isinstance(err, commands.NoPrivateMessage):
+        return await ctx.send('Cannot grant your request. \'{}\' does not work in private messages.'.format(ctx.command.name))
+    elif isinstance(err, commands.CommandNotFound):
+        return await ctx.send('Cannot grant your request. The command, \'{}\', does not exist.'.format(ctx.command.name))
+    elif isinstance(err, commands.DisabledCommand):
+        return await ctx.send('Cannot grant your request. The command, \'{}\', has been disabled.'.format(ctx.command.name))
+    elif isinstance(err, commands.CommandInvokeError):
+        err = getattr(err, 'original', err)
+        return await ctx.send('Cannot grant your request. \'{}\' raised an exception: {}.'.format(ctx.command.name, err))
+    elif isinstance(err, commands.MissingPermissions):
+        return await ctx.send('Cannot grant your request. You lack the permissions to run \'{}\'.'.format(ctx.command.name))
+    elif isinstance(err, commands.MissingRole):
+        return await ctx.send('Cannot grant your request. You lack the correct Role to run \'{}\'.'.format(ctx.command.name))
+    elif isinstance(err, commands.ExtensionError):
+        return await ctx.send('Cannot grant your request. An ExtensionError has occurred.')
+    elif isinstance(err, commands.CheckFailure):
+        return await ctx.send('Cannot grant your request. CheckFailure with \'{}\'. Do not have correct permissions.'.format(ctx.command.name))
+    elif isinstance(err, commands.UserInputError):
+        return await ctx.send('Cannot grant your request. There has been an error in your input.')
+    elif isinstance(err, commands.CommandError):
+        return await ctx.send('Cannot grant your request. A CommandError has occurred when running the \'{}\' command.'.format(ctx.command.name))
+    else:
+        return await ctx.send('Cannot grant your request. The following exception has been raised: \'{}\'.'.format(ctx.command.name))
+
 
 # '!test' command
 @bot.command(name='test', help='Repeats given arguments')
